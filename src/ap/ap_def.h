90ba0a5ec42d27136f7674b2329483b6fdf51ce0
Kei <opusk44@gmail.com>
2022-03-10 23:27:38 +0900

    move global ethernet buffers to EventSocket memeber

--- src/ap/ap_def.h
+++ src/ap/ap_def.h
@@ -14,7 +14,6 @@
 /* Const parameters for Ethernet */
 const bool DHCP_ENABLED = false;
 const std::array<uint8_t, 6> MAC_ADDRESS = {0x00, 0x08, 0xDC, 0x12, 0x34, 0x56};
-const uint16_t ETH_SHARED_BUFFER_SIZE = 2048;
 const uint16_t SOCKET_PORT_DEFAULT = 4444;
 
 #endif /* SRC_AP_AP_DEF_H_ */
--- src/ap/include/EventSocket.hpp
+++ src/ap/include/EventSocket.hpp
@@ -2,6 +2,8 @@
 #include <cstdbool>
 #include <cstdint>
 
+const uint16_t ETH_SHARED_BUFFER_SIZE = 512;
+
 enum class SocketMode {
   UDP_PEER,
   TCP_SERVER,
@@ -9,7 +11,7 @@ enum class SocketMode {
 
 class EventSocket {
  public:
-  EventSocket(uint16_t port, qbuffer_t& sendBuffer, qbuffer_t& recvBuffer, SocketMode mode);
+  EventSocket(uint16_t port, SocketMode mode);
   ~EventSocket();
 
   bool IsOpened() const;
@@ -17,13 +19,18 @@ class EventSocket {
   void OnReadable();
   void OnWritable();
 
+  qbuffer_t* GetSendBuffer(); // or Write() and AvailableWrite()
+  qbuffer_t* GetRecvBuffer(); // or Read() and Available()
+
  private:
   uint8_t fd;
   SocketMode mode_;
   uint16_t port_;
   uint16_t remotePort_;
   uint8_t remoteIpAddr_[4];
-  qbuffer_t& sendBuffer_;
-  qbuffer_t& recvBuffer_;
   bool isOpened_;
+  qbuffer_t sendBuffer_;
+  qbuffer_t recvBuffer_;
+  uint8_t sharedSendBuffer_[ETH_SHARED_BUFFER_SIZE];
+  uint8_t sharedRecvBuffer_[ETH_SHARED_BUFFER_SIZE];
 };
